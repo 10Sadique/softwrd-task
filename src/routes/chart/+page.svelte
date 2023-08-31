da6c644ff1386d9456cd7df4cce69b42032ee3e1
10Sadique <jafarsjahan@gmail.com>
2023-09-01 04:32:22 +0600

    components added

--- src/routes/chart/+page.svelte
+++ src/routes/chart/+page.svelte
@@ -1,3 +1,15 @@
-<div>
-	<h1>Chart Page</h1>
+<script>
+	import { countries } from '../../stores/countryDataStore';
+	import Table from '$lib/components/chart/Table.svelte';
+	import PolarAreaChart from '$lib/components/chart/PolarAreaChart.svelte';
+
+	export let data;
+
+	// @ts-ignore
+	countries.set(data.data);
+</script>
+
+<div class="grid grid-cols-3 gap-8">
+	<Table />
+	<PolarAreaChart />
 </div>
