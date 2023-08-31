67e4a9b27b99ab8dfcb3f2e92daa8d7965563c43
10Sadique <jafarsjahan@gmail.com>
2023-09-01 02:11:34 +0600

    grids added

--- src/routes/layout/+page.svelte
+++ src/routes/layout/+page.svelte
@@ -1,1 +1,22 @@
-<div><h1>Layout Page</h1></div>
+<script>
+	import {
+		GridOne,
+		GridTwo,
+		GridThree,
+		GridFour,
+		GridFive,
+		GridSix,
+		GridSeven
+	} from '$lib/components/layout';
+</script>
+
+<div class="grid h-screen grid-cols-4 grid-rows-6 gap-0 md:grid-cols-12">
+	<GridOne />
+	<GridTwo />
+	<GridThree />
+	<GridFour />
+
+	<GridFive />
+	<GridSix />
+	<GridSeven />
+</div>
