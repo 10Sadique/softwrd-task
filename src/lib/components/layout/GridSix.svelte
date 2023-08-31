435833dc4ff2d0625b7a06a0b0ccc5585f1f3147
10Sadique <jafarsjahan@gmail.com>
2023-09-01 02:25:44 +0600

    sticker component added

--- src/lib/components/layout/GridSix.svelte
+++ src/lib/components/layout/GridSix.svelte
@@ -1,1 +1,7 @@
-<div class="col-span-4 row-span-3 bg-yellow-100 md:col-start-7 md:row-span-4 md:row-start-3" />
+<script>
+	import { Sticker } from '$lib/components/layout';
+</script>
+
+<div class="col-span-4 row-span-3 bg-yellow-100 md:col-start-7 md:row-span-4 md:row-start-3">
+	<Sticker />
+</div>
