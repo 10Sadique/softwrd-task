7269ed76a8584610aeb7e2793bb2e61b0905dddc
10Sadique <jafarsjahan@gmail.com>
2023-09-01 01:22:44 +0600

    root page updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,19 @@
-<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
+<script>
+	const routes = [
+		{ id: 1, label: 'Layout', route: '/layout' },
+		{ id: 2, label: 'Chart', route: '/chart' },
+		{ id: 3, label: 'Map', route: '/map' }
+	];
+</script>
+
+<div class="h-screen flex flex-col items-center justify-center">
+	<!-- <h1 class="text-3xl font-bold mb-6">Softwrd Assesment</h1> -->
+	<div class="space-x-4 font-semibold text-lg">
+		{#each routes as route}
+			<a class="hover:underline" href={route.route}>{route.label}</a>
+		{/each}
+	</div>
+</div>
 
 <style lang="postcss">
 	:global(html) {
