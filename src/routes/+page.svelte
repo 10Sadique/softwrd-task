bc8ef2908b3da5402e8b637b4aad0dbe879dfa14
10Sadique <jafarsjahan@gmail.com>
2023-09-01 01:24:59 +0600

    styles moved to root layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,15 @@
 <script>
 	const routes = [
-		{ id: 1, label: 'Layout', route: '/layout' },
-		{ id: 2, label: 'Chart', route: '/chart' },
-		{ id: 3, label: 'Map', route: '/map' }
+		{ label: 'Layout', route: '/layout' },
+		{ label: 'Chart', route: '/chart' },
+		{ label: 'Map', route: '/map' }
 	];
 </script>
 
 <div class="h-screen flex flex-col items-center justify-center">
-	<!-- <h1 class="text-3xl font-bold mb-6">Softwrd Assesment</h1> -->
 	<div class="space-x-4 font-semibold text-lg">
 		{#each routes as route}
 			<a class="hover:underline" href={route.route}>{route.label}</a>
 		{/each}
 	</div>
 </div>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
