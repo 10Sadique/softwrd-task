6c4b4bead7771b6fa2c6833e1ad14059d9e172a7
10Sadique <jafarsjahan@gmail.com>
2023-09-02 01:06:07 +0600

    adding currency and language to country data

--- src/routes/chart/+page.svelte
+++ src/routes/chart/+page.svelte
@@ -5,7 +5,21 @@
 	import { countries } from '../../stores/countryDataStore';
 	import PolarAreaChart from '$lib/components/chart/PolarAreaChart.svelte';
 
-	countries.set($page.data.data);
+	const data = $page.data.data.slice(0, -1).map((country) => ({
+		...country,
+		currency: country?.currencies
+			? Object.keys(country.currencies).length > 1
+				? Object.keys(country.currencies).join(', ')
+				: Object.keys(country.currencies).toString()
+			: null,
+		language: country?.languages
+			? Object.values(country.languages).length > 1
+				? Object.values(country.languages).join(', ')
+				: Object.values(country.languages).toString()
+			: null
+	}));
+
+	countries.set(data);
 </script>
 
 <div class="grid grid-cols-3 gap-8">
